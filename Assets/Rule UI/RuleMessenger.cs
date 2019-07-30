ac2f763a10e0e83a2af0bb8590942d4011a5d964
DivineErdrick <40323567+DivineErdrick@users.noreply.github.com>
2019-07-30 17:05:16 -0500

    Begun work on Rule Messenger

--- Assets/Rule UI/RuleMessenger.cs
+++ Assets/Rule UI/RuleMessenger.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class RuleMessenger : MonoBehaviour
 {
+    public RuleSetter setter;
+
+    string startingText;
+
+    void Awake()
+    {
+        Assert.IsNotNull(setter, "The Rule Messenger is not connected to the Rule Setter.");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingText = gameObject.GetComponent<Text>().text;
     }
 
     // Update is called once per frame
@@ -19,6 +29,22 @@ public class RuleMessenger : MonoBehaviour
 
     public void DisplayMessage ()
     {
-
+        if (setter.InputName.Length == 0 || 
+          (!setter.UseDeployment &&
+           !setter.UseStartOfGame &&
+           !setter.UseYourTurn &&
+           !setter.UseOpponentsTurn &&
+           !setter.UseStartOfTurn &&
+           !setter.UseMove &&
+           !setter.UsePsychic &&
+           !setter.UseShooting &&
+           !setter.UseCharge &&
+           !setter.UseFight &&
+           !setter.UseMorale &&
+           !setter.UseEndOfTurn &&
+           !setter.UseEndOfGame))
+        {
+            gameObject.GetComponent<Text>().text = startingText;
+        }
     }
 }
