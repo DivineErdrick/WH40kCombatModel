50eba9a2e7044d1643033888f89502ec1ff160c8
DivineErdrick <40323567+DivineErdrick@users.noreply.github.com>
2019-08-01 16:59:12 -0500

    More work on the Rule UI Messenger.

--- Assets/Rule UI/RuleMessenger.cs
+++ Assets/Rule UI/RuleMessenger.cs
@@ -46,5 +46,52 @@ public class RuleMessenger : MonoBehaviour
         {
             gameObject.GetComponent<Text>().text = startingText;
         }
+        else if (setter.ActivationType == 0)
+        {
+            gameObject.GetComponent<Text>().text =
+            "Select when the rule is activated.";
+        }
+        else if (setter.RuleTarget == 0)
+        {
+            if (setter.ActivationType == 3)
+            {
+                gameObject.GetComponent<Text>().text =
+                "Select what triggers the rule to activate and what the rule targets.";
+            }
+            else
+            {
+                gameObject.GetComponent<Text>().text =
+                "Select what the rule targets.";
+            }
+        }
+        else if (setter.RuleTarget == 11 && setter.InputKeyword.Length == 0)
+        {
+            gameObject.GetComponent<Text>().text =
+            "Input the Keyword affected by the rule.";
+        }
+        else if (setter.RuleType == 0)
+        {
+            gameObject.GetComponent<Text>().text =
+            "Select the type of rule.";
+        }
+        else if (setter.RuleTarget != 7 &&
+                 setter.RuleTarget != 8 &&
+                 setter.RuleTarget != 9 &&
+                 setter.RuleTarget != 10 &&
+                 setter.Range == 0)
+        {
+            gameObject.GetComponent<Text>().text =
+            "Input a range for the rule.";
+        }
+        else 
+        {
+            switch (setter.RuleType)
+            {
+                case 1:
+                    gameObject.GetComponent<Text>().text =
+                    "Select whether the reserve restrictions and range.";
+                    break;
+            }
+        }
     }
 }
